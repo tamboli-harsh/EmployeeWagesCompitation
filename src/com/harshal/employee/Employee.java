7f93fbc8753d7100c497afc302ac998db55bb078
tamboli-harsh <tamboliharsh57@gmail.com>
2024-02-16 15:35:07 +0530

    [MODIFIED]:harshal this program checkout in present and absent

--- src/com/harshal/employee/Employee.java
+++ src/com/harshal/employee/Employee.java
@@ -1,10 +1,20 @@
 package com.harshal.employee;
-
+import java.util.Random;
 public class Employee {
 	public static void main(String [] args) {
-		
+		int attendance;
 		System.out.println("Welcome to Employee Wage Computation on master Branch");
+		Random random=new Random();
+		attendance=random.nextInt(9)%2;
 		
+		System.out.println(attendance);
+		if(attendance==0) {
+			
+			System.out.println("Employee is Absent");
+		}
+		else{
+			System.out.println("Employee is present");
+		}
 		
 	}
 	
