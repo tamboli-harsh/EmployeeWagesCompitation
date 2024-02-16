d5ee2fbbbc8478355a021a40404702236d6352c7
tamboli-harsh <tamboliharsh57@gmail.com>
2024-02-16 14:36:57 +0530

    [ADDED]:harshal Well come employe message

---
+++ src/com/har/demojava/Add.java
@@ -0,0 +1,12 @@
+package com.har.demojava;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello YOGESH");
+
+	}
+
+}
---
+++ src/com/har/demojava/Factoreal.java
@@ -0,0 +1,10 @@
+package com.har.demojava;
+
+public class Factoreal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
