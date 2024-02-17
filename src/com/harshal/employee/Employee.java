8ac032fc8e03d018d09282db766ff459903548b8
tamboli-harsh <tamboliharsh57@gmail.com>
2024-02-17 21:25:40 +0530

    [MODIFIED]:harshal -uc -1 complit for EmployeeAttendance

--- src/com/harshal/employee/Employee.java
+++ src/com/harshal/employee/Employee.java
@@ -3,9 +3,15 @@ import java.util.Random;
 public class Employee {
 	public static void main(String [] args) {
 		int attendance;
+		final int wege_per_hr=20;
+		final int full_day_hr=8;
+		int dayly_wege;
+		
 		System.out.println("Welcome to Employee Wage Computation on master Branch");
 		Random random=new Random();
 		attendance=random.nextInt(9)%2;
+		
+	
 		
 		System.out.println(attendance);
 		if(attendance==0) {
@@ -14,6 +20,9 @@ public class Employee {
 		}
 		else{
 			System.out.println("Employee is present");
+			dayly_wege=	wege_per_hr *full_day_hr;
+			
+			System.out.println(dayly_wege);
 		}
 		
 	}
